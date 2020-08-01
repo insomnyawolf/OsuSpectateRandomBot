0b55e9aabbf5c688988b53a6f8e0d0187aa18adb
ruben diaz <ruben.diaz@metrica.es>
2020-08-01 03:23:22 +0200

    Initial Code Commit

---
+++ OsuSpectateRandomBot/Models.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace OsuSpectateRandomBot
+{
+    public class Location
+    {
+        [JsonPropertyName("latitude")]
+        public double Latitude { get; set; }
+
+        [JsonPropertyName("longitude")]
+        public double Longitude { get; set; }
+
+        [JsonPropertyName("country_code")]
+        public int CountryCode { get; set; }
+    }
+
+    public class Beatmap
+    {
+        [JsonPropertyName("md5")]
+        public string Md5 { get; set; }
+
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+    }
+
+    public class Action
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+
+        [JsonPropertyName("beatmap")]
+        public Beatmap Beatmap { get; set; }
+
+        [JsonPropertyName("mods")]
+        public int Mods { get; set; }
+
+        [JsonPropertyName("game_mode")]
+        public int GameMode { get; set; }
+    }
+
+    public class Client
+    {
+        [JsonPropertyName("api_identifier")]
+        public string ApiIdentifier { get; set; }
+
+        [JsonPropertyName("type")]
+        public int Type { get; set; }
+
+        [JsonPropertyName("user_id")]
+        public int UserId { get; set; }
+
+        [JsonPropertyName("location")]
+        public Location Location { get; set; }
+
+        [JsonPropertyName("username")]
+        public string Username { get; set; }
+
+        [JsonPropertyName("privileges")]
+        public int Privileges { get; set; }
+
+        [JsonPropertyName("silence_end_time")]
+        public object SilenceEndTime { get; set; }
+
+        [JsonPropertyName("action")]
+        public Action Action { get; set; }
+
+        [JsonPropertyName("bancho_privileges")]
+        public int BanchoPrivileges { get; set; }
+    }
+
+    public class CRippleResponse
+    {
+        [JsonPropertyName("code")]
+        public int Code { get; set; }
+
+        [JsonPropertyName("clients")]
+        public Dictionary<string, List<Client>> Users { get; set; }
+
+        [JsonPropertyName("connected_users")]
+        public int ConnectedUsers { get; set; }
+
+        [JsonPropertyName("connected_clients")]
+        public int ConnectedClients { get; set; }
+    }
+
+    public enum ActionType
+    {
+        Idle = 0,
+        Afk = 1,
+        Playing = 2,
+        EditingBeatmap = 3,
+        ModdingBeatmap = 4,
+        InMultiplayerMatch = 5,
+        Watching = 6,
+        UNUSED = 7,
+        TestingBeatmap = 8,
+        SubmittingBeatmap = 9,
+        Paused = 10,
+        InMultiplayerLobby = 11,
+        PlayingMultiplayerMatch = 12,
+        UsingDirect = 13,
+    }
+
+    public enum ClientType
+    {
+        Game = 0,
+        IRC,
+        Fake,
+        Web,
+    }
+}
---
+++ OsuSpectateRandomBot/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace OsuSpectateRandomBot
+{
+    internal static class Program
+
+    {
+        private static async Task Main()
+        {
+            var BanchoHttpClient = new BanchoHttpClient();
+            var Random = new Random();
+
+            Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs e) =>
+            {
+                e.Cancel = true;
+                BanchoHttpClient.Dispose();
+                Environment.Exit(0);
+            };
+
+            while (true)
+            {
+                List<Client> PlayersPlaying = new List<Client>();
+
+                var response = await BanchoHttpClient.GetAsync().ConfigureAwait(false);
+
+                foreach (var user in response.Users)
+                {
+                    foreach (var client in user.Value)
+                    {
+                        if (client.Type == (int)ClientType.Game
+                         && client.Action.Id == (int)ActionType.Playing)
+                        {
+                            PlayersPlaying.Add(client);
+                        }
+                    }
+                }
+
+                if (PlayersPlaying.Count == 0)
+                {
+                    Console.WriteLine("Nobody Is Playing...");
+                    await Task.Delay(new TimeSpan(hours: 0, minutes: 5, seconds: 0)).ConfigureAwait(false);
+
+                    // Skips to the next iteration of the loop
+                    continue;
+                }
+
+                var selected = PlayersPlaying[Random.Next(0, PlayersPlaying.Count - 1)];
+
+
+                await Task.Delay(new TimeSpan(hours: 0, minutes: 5, seconds: 0)).ConfigureAwait(false);
+            }
+        }
+    }
+
+    public class BanchoHttpClient
+    {
+        private const string Url = "http://c.ripple.moe/api/v2/clients";
+        private HttpClient HttpClient;
+
+        public BanchoHttpClient()
+        {
+            HttpClient = new HttpClient
+            {
+                BaseAddress = new Uri(Url)
+            };
+
+            // Add an Accept header for JSON format.
+            HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<CRippleResponse> GetAsync()
+        {
+            var response = await HttpClient.GetAsync(string.Empty).ConfigureAwait(false);
+
+            // List data response.
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.Error.Write(response.StatusCode);
+                Environment.Exit(1);
+            }
+
+            var dataString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            // Parse the response body.
+            //Make sure to add a reference to System.Net.Http.Formatting.dll
+
+            return JsonSerializer.Deserialize<CRippleResponse>(dataString);
+        }
+
+        public void Dispose()
+        {
+            HttpClient.Dispose();
+        }
+    }
+}
